cf85b7ee3d325607aa0e1c5c6928d365563e60ba
anupampal404 <anupampal404@gmail.com>
2024-07-21 21:44:03 +0530

    something to be done

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,11 @@ const app = express()
 app.use(cors());
 const PORT = 3000;
 //body-parser
-app.use(express.json());
+app.use(express.json({
+    orign : ["money-wallet-client.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true
+}));
 
 app.use("/api/v1", rootRouter);
 
