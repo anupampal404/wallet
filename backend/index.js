b6e9089e31251b48d627bcdb50664abaac28c5e9
anupampal404 <anupampal404@gmail.com>
2024-07-25 23:12:08 +0530

    changing port definition

--- backend/index.js
+++ backend/index.js
@@ -3,16 +3,12 @@ const cors = require('cors')
 const rootRouter = require("./routes/index")
 const app = express()
 app.use(cors());
-const PORT = 3000;
+const port = process.env.PORT || 3000;
 //body-parser
-app.use(express.json({
-    orign : ["money-wallet-client.vercel.app"],
-    methods: ["POST", "GET"],
-    credentials: true
-}));
+app.use(express.json());
 
 app.use("/api/v1", rootRouter);
 
 app.listen(PORT,()=>{
-    console.log(`server running on port ${PORT}`)
+    console.log(`server running on port ${port}`)
 });
